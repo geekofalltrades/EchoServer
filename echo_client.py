cdeef280c1f1962a8cfb98121e6843e4958f338f
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-17 20:00:26 -0800

    Echo client now has functionality encapsulated in function 'call_echo_server', which is necessary for lettuce testing. Command line functionality has been enclosed in an if __name__ == '__main__' block so as to not corrupt imports.

--- echo_client.py
+++ echo_client.py
@@ -1,30 +1,36 @@
 import socket
 import sys
 
-#Set up the client socket.
-client_socket = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_IP)
 
-client_socket.connect(('127.0.0.1', 50000))
+def call_echo_server(msg):
+    #Set up the client socket.
+    client_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
 
-if len(sys.argv) < 2:
-    msg = raw_input("Enter your message.")
-else:
-    msg = sys.argv[1]
+    client_socket.connect(('127.0.0.1', 50000))
 
-client_socket.sendall(msg)
-client_socket.shutdown(socket.SHUT_WR)
+    client_socket.sendall(msg)
+    client_socket.shutdown(socket.SHUT_WR)
 
-buffsize = 4096
-done = False
-msg = ''
-while not done:
-    msg_part = client_socket.recv(buffsize)
-    if len(msg_part) < buffsize:
-        done = True
-    msg += msg_part
+    buffsize = 4096
+    done = False
+    msg = ''
+    while not done:
+        msg_part = client_socket.recv(buffsize)
+        if len(msg_part) < buffsize:
+            done = True
+        msg += msg_part
 
-print(msg)
-client_socket.close()
+    print(msg)
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        msg = raw_input("Enter your message.")
+    else:
+        msg = sys.argv[1]
+
+    call_echo_server(msg)
